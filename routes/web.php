10b36ef01e36e48c975f7b8d4d26aa369cd0164b
Araccelli Zevallos <araccelli.zevallos@gmail.com>
2025-09-01 03:15:27 -0500

    fix: actualiza configuracion pages

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,10 @@
 <?php
 
-use App\Models\Course;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route('courses.index');
 })->name('home');
-
-Route::get('/{any}', function () {
-    $countParents = Course::whereNull('parent_id')->count();
-    $countCourses = Course::whereNotNull('parent_id')->count();
-    $countVisibles = Course::whereNotNull('parent_id')->where('is_visible', true)->count();
-
-    return Inertia::render('courses/Courses', [
-        'summary' => [
-            'parents' => $countParents,
-            'courses' => $countCourses,
-            'visibles' => $countVisibles,
-        ]
-    ]);
-})->where('any', '.*');
-
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/configs.php';
