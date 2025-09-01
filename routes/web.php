e1466c7ee5ca3be49c93e1da283305d9e067c8b6
Araccelli Zevallos <araccelli.zevallos@gmail.com>
2025-09-01 02:36:35 -0500

    fix: agrega catcher de rutas

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,11 @@ Route::get('/', function () {
     return redirect()->route('courses.index');
 })->name('home');
 
+Route::get('{any}', function () {
+    return view('app'); // tu vista SPA
+})->where('any', '.*');
+
+
 require __DIR__ . '/auth.php';
 require __DIR__ . '/configs.php';
 require __DIR__ . '/courses.php';
