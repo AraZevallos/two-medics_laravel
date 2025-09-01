051822a50e54ed2d4dd8f9fd5ed624fe8ca57529
Araccelli Zevallos <araccelli.zevallos@gmail.com>
2025-09-01 02:49:54 -0500

    fix: agrega redireccion de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Models\Course;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return redirect()->route('courses.index');
 })->name('home');
 
-Route::get('{any}', function () {
-    return view('app'); // tu vista SPA
+Route::get('/{any}', function () {
+    $countParents = Course::whereNull('parent_id')->count();
+    $countCourses = Course::whereNotNull('parent_id')->count();
+    $countVisibles = Course::whereNotNull('parent_id')->where('is_visible', true)->count();
+
+    return Inertia::render('courses/Courses', [
+        'summary' => [
+            'parents' => $countParents,
+            'courses' => $countCourses,
+            'visibles' => $countVisibles,
+        ]
+    ]);
 })->where('any', '.*');
 
 
